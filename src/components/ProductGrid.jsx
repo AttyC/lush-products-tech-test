a087d89a3be6bd2c22dfcfa529bf9c9cb949afd2
Atty Cronin <antoniajcronin@gmail.com>
2023-05-08 15:39:31 +0100

    renamed the sorting correctly

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -28,59 +28,59 @@ export const ProductGrid = (data) => {
 
   return (
     <>
-      <div className='flex flex-wrap justify-around container items-between lg:bg-gray-100 container lg:my-16'>
-        <div className='btn-group flex flex-col price-btn'>
+      <div className='flex flex-wrap justify-around container items-between container lg:my-16 bg-gray-50 flex-col sm:flex-row'>
+        <div className='btn-group flex flex-col price-btn items-center  '>
           <button
             onClick={() => handleSorting("price")}
-            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-100 transition-colors text-gray-700'
-          >
-            Sort By Price (High - Low)
-          </button>
-          <button
-            onClick={() => handleSorting("-price")}
-            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-100 transition-colors text-gray-700'
+            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 md:w-auto border-b-2 border-gray-300 hover:bg-gray-200 transition-colors text-gray-700'
           >
             Sort By Price (Low - High)
           </button>
+          <button
+            onClick={() => handleSorting("-price")}
+            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-200 transition-colors text-gray-700'
+          >
+            Sort By Price (High - Low)
+          </button>
         </div>
-        <div className='btn-group flex flex-col'>
+        <div className='btn-group flex flex-col rating-btn items-center'>
           <button
             onClick={() => handleSorting("-rating")}
-            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-100 transition-colors text-gray-700'
+            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-200 transition-colors text-gray-700'
           >
             Sort By Rating (High - Low)
           </button>
           <button
             onClick={() => handleSorting("rating")}
-            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-100 transition-colors text-gray-700'
+            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-200 transition-colors text-gray-700'
           >
             Sort By Rating (Low - High)
           </button>
         </div>{" "}
-        <div className='btn-group flex flex-col category-btn'>
+        <div className='btn-group flex flex-col category-btn items-center'>
           <button
             onClick={() => handleSorting("category")}
-            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-100 transition-colors text-gray-700'
+            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-200 transition-colors text-gray-700'
           >
             Sort By Category (A-Z)
           </button>
           <button
             onClick={() => handleSorting("-category")}
-            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-100 transition-colors text-gray-700'
+            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-200 transition-colors text-gray-700'
           >
             Sort By Category (Z-A)
           </button>
         </div>
-        <div className='btn-group flex flex-col name-btn'>
+        <div className='btn-group flex flex-col name-btn items-center'>
           <button
-            onClick={() => handleSorting("name")}
-            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-100 transition-colors text-gray-700'
+            onClick={() => handleSorting("-name")}
+            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-200 transition-colors text-gray-700'
           >
             Sort By Name (A-Z)
           </button>
           <button
-            onClick={() => handleSorting("-name")}
-            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-100 transition-colors text-gray-700'
+            onClick={() => handleSorting("name")}
+            className='font-medium text-sm px-4 py-2 lg:m-4 m-2 w-full md:w-auto border-b-2 border-gray-300 hover:bg-gray-200 transition-colors text-gray-700'
           >
             Sort By Name (Z-A)
           </button>
